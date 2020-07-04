fef6335cdddb65843678b3f9d98e068e3e152cf5
tholt1220 <trevholt1220@gmail.com>
2020-07-03 22:10:11 -0700

    Deleted Excess Files

--- database/models.py
+++
@@ -1,7 +0,0 @@
-# models.py
-from .db import db
-
-class Song(db.Document):
-    title = db.StringField(required=True)
-    tempo = db.IntField(required=True)
-    filename = db.StringField(required=True)
